3b7a94dc53d238889209c334f635ed2fa88a8e73
KimrAndrew <andrewrk1997@gmail.com>
2021-10-25 14:52:42 -0700

    Header Componant rendering properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import Header from "Header.js";
+import Header from "./Header.js";
 import Main from "./Main.js";
 //import Footer from "Footer.js";
 
@@ -7,6 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Main />
       </div>
     )
