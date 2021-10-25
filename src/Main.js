a3059c9529fc195ba033a48be39d306074ddb67a
KimrAndrew <andrewrk1997@gmail.com>
2021-10-25 14:38:33 -0700

    initialized Main and HornedBeast components

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import HornedBeast from "HornedBeast.js";
+
+class Main extends Component {
+    render() {
+        return (
+            <>
+                <HornedBeast imageUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'} title={"Uniwhal"} description={"A unicorn and a narwhal nuzzling their horns"} />
+            </>
+        )
+    }
+}
+
+export default Main;
