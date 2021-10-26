e2fc9cafb1e9450776a16a0698f4a895f5790540
KimrAndrew <andrewrk1997@gmail.com>
2021-10-26 16:30:04 -0700

    completed Feature # 2, HornedBeast components now have number of favorites stored in their state, clicking on an image will cause the number next to the heart below it to increment by one

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,25 @@
 import { Component } from "react";
 
 class HornedBeast extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            favorites: 0
+        }
+    }
+
+    favorite = () => {
+        this.setState({favorites: this.state.favorites + 1});
+    }
+
     render() {
         return (
             <>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} />
+                <img onClick={this.favorite} src={this.props.imageUrl} alt={this.props.description} title={this.props.title} />
                 <p>{this.props.description}</p>
+                <p><span>❤️</span><span>{this.state.favorites}</span></p>
             </>
         )
     }
